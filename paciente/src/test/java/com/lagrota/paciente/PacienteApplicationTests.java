99da3f58ec0f8fe254c42b00d1f0aabc55af44b6
LagrotaGabriel <gabriellagrota23@gmail.com>
2024-11-17 18:58:46 -0300

    [MAIN] feat: Primeiro commit

---
+++ paciente/src/test/java/com/lagrota/paciente/PacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagrota.paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
