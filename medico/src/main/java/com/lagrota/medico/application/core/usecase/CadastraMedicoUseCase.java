0d1c01929416db971f4b4ec08e88cb1ed6ec2684
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-11-19 11:12:34 -0300

    [MAIN] [MEDICO] feat: Criado o caso de uso para cadastro de novo médico

---
+++ medico/src/main/java/com/lagrota/medico/application/core/usecase/CadastraMedicoUseCase.java
@@ -0,0 +1,23 @@
+package com.lagrota.medico.application.core.usecase;
+
+import com.lagrota.medico.application.core.domain.Medico;
+import com.lagrota.medico.application.ports.out.CadastraMedicoOutputPort;
+
+/**
+ * CadastraMedicoUseCase
+ * Caso de uso de cadastro de novo médico
+ * @author Gabriel Henrique Afonso Cruz Lagrota
+ * @since 19/11/2024 - 11:05
+ */
+public class CadastraMedicoUseCase {
+
+    private final CadastraMedicoOutputPort cadastraMedicoOutputPort;
+
+    public CadastraMedicoUseCase(CadastraMedicoOutputPort cadastraMedicoOutputPort) {
+        this.cadastraMedicoOutputPort = cadastraMedicoOutputPort;
+    }
+
+    public void cadastra(Medico medico) {
+        cadastraMedicoOutputPort.cadastra(medico);
+    }
+}
